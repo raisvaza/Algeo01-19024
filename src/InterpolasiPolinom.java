65f31a80520ea338db3d9d9e61d7ce19e0c10052
Rais Vaza <raisvaza@gmail.com>
2020-10-01 02:47:02 +0700

    Interpolasi Polinom beres banget

--- src/InterpolasiPolinom.java
+++ src/InterpolasiPolinom.java
@@ -1,8 +1,8 @@
 import java.lang.Math;
 
 public class InterpolasiPolinom extends Matriks{
-    private int derajat; //derajat polinom
-    private double [] solusi;
+    public int derajat; //derajat polinom
+    public double [] solusi;
 
     public void setderajat(int x){
         this.derajat = x;
@@ -13,40 +13,40 @@ public class InterpolasiPolinom extends Matriks{
     }
 
     //KONSTRUKTOR
-    public InterpolasiPolinom(int d, double x){
+    public InterpolasiPolinom(int d){
         derajat = d;
         Matriks = new double[derajat+1][derajat+2];
-        solusi = new double[derajat];
+        solusi = new double[derajat+2];
     }
 
     public void MatriksKordinatToPolinom(Matriks M){
         /*Mengubah Matriks kordinat ke matriks polinom */
         int i,j,k;
         double [] [] temporary;
-        temporary = new double[this.derajat+1][this.derajat+2];
-        k = 0;
+        temporary = new double[this.derajat+2][this.derajat+3];
+        k = 1;
 
-        for (i = 0; i < M.brs; i++)
+        for (i = 1; i <= M.brs; i++)
         {
-            for (j = 0; j < M.kol; j++)
+            for (j = 1; j <= M.kol; j++)
             {
-                if (j == 0)
+                if (j == 1)
                 {
-                  for (k = 0; k < derajat+1; k++)
+                  for (k = 1; k <= derajat+1; k++)
                   {
-                    temporary[i][k] = Math.pow(M.GetElmt(i, j),k);
+                    temporary[i][k] = Math.pow(M.GetElmt(i, j),k-1);
                   }
-                } else if (j == 1)
+                } else if (j == 2)
                 {
-                    temporary[i][derajat+1] = M.GetElmt(i, j);
+                    temporary[i][derajat+2] = M.GetElmt(i, j);
                 }
             }
         }
 
         M.SetBrs(this.derajat+1);
         M.SetKol(this.derajat+2);
-        for (i = 0; i < M.GetBrs(); i++){
-            for (j = 0; j < M.GetKol(); j++){
+        for (i = 1; i <= M.GetBrs(); i++){
+            for (j = 1; j <= M.GetKol(); j++){
                 M.SetElmt(i, j, temporary[i][j]);
             }
         }
@@ -54,10 +54,10 @@ public class InterpolasiPolinom extends Matriks{
 
     public void PolinomSolusi(Matriks MGaussJordan){
         int i;
-        solusi = new double [this.derajat+1];
+        solusi = new double [this.derajat+2];
 
-        for (i = 0 ; i <= MGaussJordan.GetBrs()-1; i++){
-            solusi[i] = MGaussJordan.GetElmt(i, MGaussJordan.GetKol()-1);
+        for (i = 1 ; i <= MGaussJordan.GetBrs(); i++){
+            solusi[i] = MGaussJordan.GetElmt(i, MGaussJordan.GetKol());
         }
     }
 
@@ -66,8 +66,8 @@ public class InterpolasiPolinom extends Matriks{
         double y;
 
         y = 0;
-        for (i = 0; i < derajat+1; i++){
-            y += Math.pow(x,i)*solusi[i];
+        for (i = 1; i <= derajat+1; i++){
+            y += Math.pow(x,i-1)*solusi[i];
         }
 
         return y;
--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ class Main {
             if (pilihan == 1) spl();
             // else if (pilihan == 2) det();
             // else if (pilihan == 3) inv();
-            // else if (pilihan == 4) interPol();
+            else if (pilihan == 4) Interpol();
             // else if (pilihan == 5) reg();
             else if(pilihan == 6) System.exit(0);
             else{
@@ -469,7 +469,65 @@ class Main {
         }
     }
 
-    static void interPol(){
-               
+    static void Interpol(){
+        System.out.println("=== Interpolasi Polinom ===");
+        System.out.println("1. Masukan keyboard");
+        System.out.println("2. Masukan file");
+        System.out.print("Masukkan pilihan: ");
+        int masukan = in.nextInt();
+        System.out.println();
+
+        Matriks M = new Matriks();
+             
+        while(true){
+            if(masukan == 1){
+                //Meminta input jumlah persamaan dan jumlah peubah
+                System.out.print("Masukkan jumlah titik diketahui: ");
+                int n = in.nextInt();
+                M.SetBrs(n);
+                M.SetKol(2);
+    
+                //Membaca persamaan
+                M.isiMatriks(n, 2);
+                System.out.println();
+                break;
+
+            } else if (masukan == 2) {
+                //Membaca matriks yang ada di file
+                String namaFile = in.nextLine();
+                File file  = new File(namaFile);
+
+                while(!file.exists()){
+                    System.out.print("Masukkan nama file: ");
+                    namaFile = in.nextLine();
+                    file = new File(namaFile);
+                }
+
+                M.bacaFile(file);
+                System.out.println();
+                break;
+
+            } else {
+                System.out.print("Masukan salah. Silakan masukkan ulang! ");
+                masukan = in.nextInt();   
+            }
+        }
+
+        int d = M.GetBrs()-1;
+        float x = in.nextInt();
+        InterpolasiPolinom InPol = new InterpolasiPolinom(d);
+        M.tulisMatriks();
+        InPol.MatriksKordinatToPolinom(M);
+        M.tulisMatriks();
+        System.out.println();
+        M.eliminasiGaussJordan();
+        M.tulisMatriks();
+        System.out.println();
+        InPol.PolinomSolusi(M);
+        for (int i =0; i <= d; i++){
+            System.out.println(InPol.solusi[i]);
+        }
+        double y = InPol.SolusiInterpolasi(x);
+        System.out.println("y = " + y);
     }
 }
