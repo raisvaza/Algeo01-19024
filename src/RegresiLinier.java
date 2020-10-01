7989aa46746b015207a945563a051a3b3216c1ec
Rais Vaza <raisvaza@gmail.com>
2020-10-01 23:36:44 +0700

    Regresi Linear selesai

--- src/RegresiLinier.java
+++ src/RegresiLinier.java
@@ -1,9 +1,46 @@
-import java.util.*;
 
 public class RegresiLinier extends Matriks{
+    public int NSampel;
+    public int NVar;
+    
+    public RegresiLinier(int N, int var){
+        this.NSampel = N;
+        this.NVar = var;
+        this.Matriks = new double[N+1][var+2];
+    }
 
-    public Scanner in = new Scanner(System.in);
-    RegresiLinier(){
+    public void SampelToRegresi(Matriks M){
+        int brs,kol,i,j,k,sum;
+        double[][] temp;
+
+        brs = M.GetBrs();
+        kol = M.GetKol();
+        temp = new double[kol][kol+1];
+
+        //nilai matriks M pada indeks baris = 0 atau indeks kolom = 0 dibuat 1
+        for (i = 0; i < brs; i++){
+            for (j = 0; j < kol; j++){
+                if (i == 0 || j == 0){
+                    M.Matriks[i][j] = 1;
+                }
+            }
+        }
+
+        //Membuat matriks Regresi Linear
+        for (i = 0; i <= kol; i++){
+            for (j = 0; j <= kol+1; j++){
+                if (i == 1 && j == 1){
+                    temp[i][j] = M.GetBrs()-1;
+                } else {
+                    sum = 0;
+                    for (k = 1; k < brs; k++){
+                        sum += M.GetElmt(k, i-1) * M.GetElmt(k, j-1);
+                    }
+                    temp[i][j] = sum;
+                }
+            }
+        }
+
 
     }
 
